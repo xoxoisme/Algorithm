f7380c3f3450f39caf94b510084c745755686df3
xoxoisme <kxoxo3333@gmail.com>
2026-03-08 23:46:07 +0900

    add: 백준 2086 완료

--- CP/Baekjoon/Gold/2086-피보나치-수의-합.py
+++ CP/Baekjoon/Gold/2086-피보나치-수의-합.py
@@ -1,18 +1,17 @@
-MOD = 1000000000
-
 def fibonacci(n):
     if n == 0:
         return (0, 1)
 
     a, b = fibonacci(n // 2)
 
-    c = (a * ((2 * b - a) % MOD)) % MOD
-    d = (a * a + b * b) % MOD
+    c = (a * (2 * b - a)) % 1000000000
+
+    d = (a * a + b * b) % 1000000000
 
     if n % 2 == 0:
         return (c, d)
     else:
-        return (d, (c + d) % MOD)
+        return (d, (c + d) % 1000000000)
 
 
 a, b = map(int, input().split())
@@ -20,4 +19,4 @@ a, b = map(int, input().split())
 fib_b = fibonacci(b + 2)[0]
 fib_a = fibonacci(a + 1)[0]
 
-print((fib_b - fib_a) % MOD)
+print((fib_b - fib_a) % 1000000000)
